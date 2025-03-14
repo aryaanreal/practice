ef672af17260b17c9d90722387a97b5279408b03
aryaanreal <aryaantalukder@gmail.com>
2025-03-14 21:53:19 +1030

    This is a function that prints the scaled version of a matrix

---
+++ scaledmatrix.cpp
@@ -0,0 +1,13 @@
+//this is a function that takes an array of matrix and prints out the scaled version
+#include <iostream>
+void print_scaled(int array[3][3],int scale) {
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      std::cout << array[i][j] * scale;
+      if (j<2) {
+        std::cout << " ";
+      }
+    }
+    std::cout << std::endl;
+  }
+}
